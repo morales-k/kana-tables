da9634e596364104044a93b9d04ff05cfe4526fc
Krista M <morales-k@outlook.com>
2023-02-09 19:50:28 -0500

    App import and styles
    Added boilerplate, app and table styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles/App.css";
+import HiraganaTable from "./Components/Hiragana/HiraganaTable";
+import KatakanaTable from "./Components/Katakana/KatakanaTable";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    	<h1>Japanese Kana reference tables</h1>
+		<HiraganaTable />
+		<KatakanaTable />
+    </>
+
+   
   );
 }
 
