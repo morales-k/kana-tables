3335a8382cb8479209ce9ffd5c5403d797839063
Krista M <morales-k@outlook.com>
2023-02-11 15:49:46 -0500

    Navigation
    Added basic nav structure. Updated styling.

---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Kana charts</li>
+            <li>Hiragana charts</li>
+            <li>Katakana charts</li>
+            <li>Hiragana practice</li>
+            <li>Katakana practice</li>
+            <li>Practice charts</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav;
