73b7a274023adcce5d66138e8e2cd918943ddab4
Krista M <morales-k@outlook.com>
2023-02-13 20:42:03 -0500

    Updated Nav
    Nav now sets a chart number to control which chart is displayed.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
+import react, { useState } from "react";
 import "./Styles/App.css";
 import Nav from "./Components/Nav";
 import HiraganaTable from "./Components/Hiragana/HiraganaTable";
 import KatakanaTable from "./Components/Katakana/KatakanaTable";
 import KatakanaTablePractice from "./Components/Katakana/Practice/KatakanaTablePractice";
 import HiraganaTablePractice from "./Components/Hiragana/Practice/HiraganaTablePractice";
+import PrintButton from "./Components/PrintButton";
 
 function App() {
-  return (
-    <div id="appContainer">
-		<Nav />
-    	<h1>Japanese Kana reference tables</h1>
-      	<HiraganaTable />
-      	<KatakanaTable />
-    </div>
+	const [chart, setChart] = useState(0);
 
-   
-  );
+	return (
+		<div id="appContainer">
+			<Nav setChart={setChart} />
+			{
+				chart === 0 ?
+				<>
+				<HiraganaTable />
+				<KatakanaTable />
+				</> :
+				chart === 1 ?
+				<HiraganaTable /> :
+				chart === 2 ?
+				<KatakanaTable /> :
+				<>
+				<PrintButton />
+				<HiraganaTablePractice />
+				<KatakanaTablePractice />
+				</>
+			}
+		</div>
+
+	
+	);
 }
 
 export default App;
